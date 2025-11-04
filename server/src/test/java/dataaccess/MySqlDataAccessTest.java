fe46a9918489685e50b7e690a394bc5eb62b6cd5
jacobkirter01 <jacobkirter01@gmail.com>
2025-11-03 19:56:46 -0700

    custom negative addUser

--- server/src/test/java/dataaccess/MySqlDataAccessTest.java
+++ server/src/test/java/dataaccess/MySqlDataAccessTest.java
@@ -1,23 +1,25 @@
 package dataaccess;
 
 import datamodel.UserData;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySqlDataAccessTest {
 
-   private MySqlDataAccess dao;
+   private static MySqlDataAccess dao;
 
    @BeforeEach
-   public void setUp() {
+   void clearTables(){
        dao = new MySqlDataAccess();
        dao.clear();
    }
 
    @Test
+   @DisplayName("addUser positive")
     public void testAddGetUser() {
        var user = new UserData("Jacob", null, "pwd");
        dao.addUser(user);
@@ -28,6 +30,16 @@ class MySqlDataAccessTest {
 
        assertNotEquals("pwd", result.password());
        assertTrue(BCrypt.checkpw("pwd", result.password()));
+   }
+
+   @Test
+   @DisplayName("addUser negative")
+   public void testAddUserDuplicate() {
+       var user = new UserData("Jacob", null, "pwd");
+       dao.addUser(user);
+       var dup = new UserData("Jacob", null, "pwd3");
+       assertThrows(RuntimeException.class, () -> dao.addUser(dup));
+
    }
 
 }
