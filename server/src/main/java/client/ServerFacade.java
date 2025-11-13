d88c5a144680c4d4b08b24b76068dfa3ef52a04d
jacobkirter01 <jacobkirter01@gmail.com>
2025-11-13 16:13:00 -0700

    more progress with ServerFacade

--- server/src/main/java/client/ServerFacade.java
+++ server/src/main/java/client/ServerFacade.java
@@ -9,6 +9,7 @@ import java.net.URI;
 import java.net.http.*;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.security.Provider;
 import java.util.Map;
 
 public class ServerFacade {
@@ -31,6 +32,29 @@ public class ServerFacade {
         return handleResponse(response, Map.class);
     }
 
+    public int createGame(String authToken, String gameName) throws ServiceException {
+        var body = Map.of("gamaName", gameName);
+        var request = buildRequest("POST", "/game", body, authToken);
+        var response = sendRequest(request);
+
+        Map result = handleResponse(response, Map.class);
+        Double id = (Double) result.get("id");
+        return id.intValue();
+    }
+
+    public void joinGame(String authToken, int gameID, String playerColor) throws ServiceException {
+        var body = Map.of("gameID", gameID, "playerColor", playerColor);
+        var request = buildRequest("PUT", "/game", body, authToken);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public Map listGames(String authToken) throws ServiceException {
+        var request = buildRequest("GET", "/game", null, authToken);
+        var response = sendRequest(request);
+        return handleResponse(response, Map.class);
+    }
+
     private HttpRequest buildRequest(String method, String path, Object body, String authToken) {
         var builder = HttpRequest.newBuilder()
                 .uri(URI.create(serverUrl + path))
@@ -49,6 +73,39 @@ public class ServerFacade {
     private HttpRequest.BodyPublisher makeRequestBody(Object request) {
         if (request == null){
             return BodyPublishers.noBody();
+        }
+        return BodyPublishers.ofString((new Gson()).toJson(request));
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ServiceException {
+        try{
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ServiceException(500, ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> type) throws ServiceException {
+        int status = response.statusCode();
+
+        if (status / 100 != 2){
+            var body = response.body();
+            throw new ServiceException(status, extractMessage(body));
+        }
+
+        if (type == null) {
+            return null;
+        }
+
+        return new Gson().fromJson(response.body(), type);
+    }
+
+    private String extractMessage(String jsonError) {
+        try{
+            Map m = new Gson().fromJson(jsonError, Map.class);
+            return(String) m.get("message");
+        } catch (Exception ex){
+            return "unknown error";
         }
     }
 }
